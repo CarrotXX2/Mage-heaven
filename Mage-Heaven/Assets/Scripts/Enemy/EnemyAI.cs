4e04cbe036df6177d728f9fb91a42d931255c922
Canpaii <cansilay70@gmail.com>
2025-05-26 01:40:17 +0200

    Tested IK
    Tried a bunch of stuff trying to make IK work. Finally got some progress with the animation rigging from unity but it needs alot of love to make it look good

--- Mage-Heaven/Assets/Scripts/Enemy/EnemyAI.cs
+++ Mage-Heaven/Assets/Scripts/Enemy/EnemyAI.cs
@@ -1,160 +1,121 @@
 using UnityEngine;
+using UnityEngine.Animations.Rigging;
 
 public class EnemyAI : MonoBehaviour
 {
     [Header("Attack Data")]
     [SerializeField] private EnemyAttackData[] attacks;
     private EnemyAttackData _currentAttack;
-    
+
     [Header("Timers")]
     [SerializeField] private float _timeBetweenAttacksMin;
     [SerializeField] private float _timeBetweenAttacksMax;
-    
+
     private float _timeBetweenAttacks;
     private float _attackTimer;
 
-    [Header("Animations")]
-    [SerializeField] private Transform _ikHand; 
-    [SerializeField] private Transform _ikTarget; // Target for inverse Kinematics 
-    [SerializeField] private float _maxIKDistance;
-    [SerializeField] private float _ikFadeDuration;
-    
+    [Header("Rigging")]
+    [SerializeField] private TwoBoneIKConstraint rightArmIK;
+    [SerializeField] private Transform _ikTarget; // IK target to follow
+    [SerializeField] private Transform _ikHint;   // Hint to guide bend direction
+    [SerializeField] private float _maxIKDistance = 2f;
+    [SerializeField] private float _ikFadeDuration = 1f;
+    [SerializeField] private float transitionSpeed = 5f;
+
     private float _ikFadeTimer;
-    
     private bool _isPunching;
     public bool isParryAble;
-    
-    private float ikWeight;
-    private float targetWeight;
-    
-    [SerializeField] private float transitionSpeed;
-    private Animator _animator;
 
     [Header("Particles")]
     [SerializeField] private GameObject _parryAura;
+
     private void Start()
     {
-        _animator = GetComponent<Animator>();
+        _timeBetweenAttacks = Random.Range(_timeBetweenAttacksMin, _timeBetweenAttacksMax);
     }
 
     private void Update()
     {
         _attackTimer += Time.deltaTime;
-        
+
         if (_attackTimer >= _timeBetweenAttacks)
         {
-            PerformAttack();
+            // PerformAttack();
         }
 
-        if (!_currentAttack.parryAble) return;
         HandleIK();
     }
-    
-    private void PerformAttack()
+
+    public void Test()
     {
-        // New attack timer after every attack
-        _timeBetweenAttacks = Random.Range(_timeBetweenAttacksMin, _timeBetweenAttacksMax);
-        
-        EnemyAttackData data = GetRandomWeightedAttack();
-        
-        _animator.SetTrigger(data.clip.name);
-    }
-    
-    private EnemyAttackData GetRandomWeightedAttack() // Returns an attackData based on the weight of the attacks
-    {
-        float totalWeight = 0f;
-
-        // Get the total weight of all attacks
-        foreach (var attack in attacks)
-        {
-            totalWeight += attack.weight;
-        }
-
-        // Pick a random number between 0 and the total weight
-        float randomValue = Random.Range(0, totalWeight);
-
-        // Go through the list and subtract weights until you find the attack
-        float cumulativeWeight = 0f;
-        foreach (var attack in attacks)
-        {
-            cumulativeWeight += attack.weight;
-
-            if (randomValue <= cumulativeWeight)
-            {
-                return attack;
-            }
-        }
-
-        return null; // Should never happen unless list is empty or weights are 0
-    }
-    
-    private void OnAnimatorIK(int layerIndex)
-    {
-        if (_currentAttack != null && _currentAttack.parryAble)
-        {
-            // Enable IK influence
-            _animator.SetIKPositionWeight(AvatarIKGoal.RightHand, ikWeight);
-            _animator.SetIKRotationWeight(AvatarIKGoal.RightHand, ikWeight);
-            
-            _animator.SetIKPosition(AvatarIKGoal.RightHand, _ikTarget.position);
-            _animator.SetIKRotation(AvatarIKGoal.RightHand, Quaternion.LookRotation(_ikTarget.position - transform.position));
-        }
-    }
-
-    #region Inverse Kinematics
-    
-    private void HandleIK()
-    {
-        float distance = Vector3.Distance(_ikHand.position, _ikTarget.position);
-
-        // If punching, build up IK weight
-        if (_isPunching)
-        {
-            _ikFadeTimer = _ikFadeDuration; // Reset fade timer
-            float target = Mathf.Clamp01(1f - (distance / _maxIKDistance));
-            ikWeight = Mathf.MoveTowards(ikWeight, target, transitionSpeed * Time.deltaTime);
-        }
-        else if (_ikFadeTimer > 0) 
-        {
-            _ikFadeTimer -= Time.deltaTime;
-
-            // Gradually fade out IK weight so the animation doesn't snap in a weird way
-            ikWeight = Mathf.MoveTowards(ikWeight, 0f, transitionSpeed * Time.deltaTime);
-        }
-        else
-        {
-            ikWeight = 0f;
-        }
-    }
-    
-    // The next 5 functions get called trough the animator for more specific timings
-    public void StartPunch() // Turn on for smooth fade in 
-    {
+        PerformAttack();
         _isPunching = true;
     }
 
-    public void PullBackPunch() // Turn off bool for smooth Fade out 
+    private void PerformAttack()
     {
-        _isPunching = false;
+        _timeBetweenAttacks = Random.Range(_timeBetweenAttacksMin, _timeBetweenAttacksMax);
+        EnemyAttackData data = GetRandomWeightedAttack();
+        _currentAttack = data;
+
+        GetComponent<Animator>().SetTrigger(data.clip.name);
+        print(data.clip.name);
     }
 
-    public void IsParryAble() // Call this to enable parry window
+    private EnemyAttackData GetRandomWeightedAttack()
+    {
+        float totalWeight = 0f;
+        foreach (var attack in attacks)
+            totalWeight += attack.weight;
+
+        float randomValue = Random.Range(0, totalWeight);
+        float cumulativeWeight = 0f;
+
+        foreach (var attack in attacks)
+        {
+            cumulativeWeight += attack.weight;
+            if (randomValue <= cumulativeWeight)
+                return attack;
+        }
+
+        return null;
+    }
+
+    private void HandleIK()
+    {
+        float distance = Vector3.Distance(rightArmIK.data.tip.position, _ikTarget.position);
+        float targetWeight = 0f;
+
+        if (_isPunching)
+        {
+            _ikFadeTimer = _ikFadeDuration;
+            targetWeight = Mathf.Clamp01(1f - (distance / _maxIKDistance));
+        }
+        else if (_ikFadeTimer > 0)
+        {
+            _ikFadeTimer -= Time.deltaTime;
+            targetWeight = 0f;
+        }
+
+        float currentWeight = rightArmIK.weight;
+        rightArmIK.weight = Mathf.MoveTowards(currentWeight, targetWeight, transitionSpeed * Time.deltaTime);
+    }
+
+    // Called by animation events
+    public void StartPunch() => _isPunching = true;
+    public void PullBackPunch() => _isPunching = false;
+
+    public void IsParryAble()
     {
         isParryAble = true;
-        
         _parryAura.SetActive(true);
     }
 
-    public void StopParryAble() // Call this to disable parry window 
+    public void StopParryAble()
     {
         isParryAble = false;
-        
-        _parryAura.SetActive(false); 
+        _parryAura.SetActive(false);
     }
 
-    public void ResetAttackData() // Call this to set attack data to null
-    {
-        _currentAttack = null;
-    }
-    #endregion
+    public void ResetAttackData() => _currentAttack = null;
 }
